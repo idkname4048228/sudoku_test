b162ddf1d682ea05496de4af7cb66a7f9bf3b1cf
kay <kay1314233@gmail.com>
2021-11-05 09:45:58 +0800

    數獨演算法完成，加入少許註解

--- object_sudo.py
+++ object_sudo.py
@@ -1,12 +1,18 @@
+import copy
+import time
+import sys
+sys.setrecursionlimit(100)
+
 class sudoku:
     
     check_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     def __init__( self, sudoku_list ):
-        self.can_solve = False
-        self.had_insert = False
-        self.Dic = {}
-        self.the_possible_place = 0
+        self.can_solve = False  #數獨能不能解
+        self.need_solve = True  #數獨需不需要解
+        self.had_insert = False  #數獨是否有新增數字
+        self.Dic = {}  #數獨每一個空格所能放的數字
+        self.the_possible_place = -1  #
         
         self.itself = sudoku_list
         self.cloumn_list = [ [], [], [], [], [], [], [], [], [] ]#橫
@@ -39,18 +45,29 @@ class sudoku:
                 self.block_list[ ( i//3 )*3 + j//3 ].append( cloumn[ j ] )
 
 
+    def need_to_solve( self ):
+        self.need_solve = True
+        the_amount_of_zeroes = 0
+        
+        for i in range( 9 ):#數數字多少
+            for j in range( 9 ):
+                if self.itself[ i ][ j ] == 0:
+                    the_amount_of_zeroes = the_amount_of_zeroes + 1
+        
+        if the_amount_of_zeroes == 0:
+            self.need_solve = False
+            
+        return self.need_solve
+
     def can_be_solve( self ):
+        self.can_solve = False
         more_num = False
         the_amount_of_numbers = 0
-        the_amount_of_zeroes = 0
         
         for i in range( 9 ):#數數字多少
             for j in range( 9 ):
                 if self.itself[ i ][ j ] != 0:
                     the_amount_of_numbers = the_amount_of_numbers + 1
-                else:#順便建字典
-                    the_amount_of_zeroes = the_amount_of_zeroes + 1
-                    self.Dic[ i*9 + j ] = []
                         
         while more_num == False:#數字重複?
             for i in range( 9 ):
@@ -60,25 +77,17 @@ class sudoku:
                     or ( list( self.block_list[ i ] ).count( self.check_numbers[ j ] ) > 1 ) ):
                         more_num = True
             break
-
-        if the_amount_of_numbers >= 17 and more_num == False and the_amount_of_zeroes >= 1:
-            for i in range( 9 ):
-                print( self.itself [ i ])
-            print( the_amount_of_zeroes )
+        
+        if the_amount_of_numbers >= 17 and the_amount_of_numbers < 81 and more_num == False:
+            
             self.can_solve = True
             
         if self.can_solve:#可解就執行
-            for i in range( 9 ):
-                for j in range( 9 ):
-                    try:
-                        if self.Dic[ i*9 + j ] == []:#先看有沒有創建，不然會自動創建
-                            
-                            self.Dic[ i*9 + j ] = self.cloumn_list[ i ] + self.row_list[ j ] + self.block_list[ ( i//3 )*3 + j//3 ]
-                            self.Dic[ i*9 + j ] = list( set( self.check_numbers ) - set( self.Dic[ i*9 + j ] ) )
-                            
-                    except:
-                        continue
-
+            self.dic_renew()
+            
+        if self.the_min_of_dic() == 0:
+            self.can_solve = False
+        
         return self.can_solve
 
     def the_min_of_dic( self ):
@@ -118,69 +127,114 @@ class sudoku:
                         
                 except:
                     continue
-            cloumn_test = list( set( cloumn_test ) )
-            row_test = list( set( row_test ) )
-            block_test = list( set( block_test ) )
+            cloumn_test = list( set( self.Dic[ key ] ) - set( cloumn_test ) )
+            row_test = list( set( self.Dic[ key ] ) - set( row_test ) )
+            block_test = list( set( self.Dic[ key ] ) - set( block_test ) )
             
-            if len( set( self.Dic[ key ] ) - set( cloumn_test ) ) == 1:
-                self.itself[ key//9 ][ key%9 ]  = list( set( self.Dic[ key ] ) - set( cloumn_test ) )[ 0 ]
+            if len( cloumn_test ) == 1:
+                self.itself[ key//9 ][ key%9 ]  = cloumn_test[ 0 ]
                 
                 self.had_insert = True
                 self.sudo_renew()
                 self.dic_renew()
-                break
             
-            elif len( set( self.Dic[ key ] ) - set( row_test ) ) == 1:
-                self.itself[ key//9 ][ key%9 ]  = list( set( self.Dic[ key ] ) - set( row_test ) )[ 0 ]
+            elif len( row_test ) == 1:
+                self.itself[ key//9 ][ key%9 ]  = row_test[ 0 ]
                 
                 self.had_insert = True
                 self.sudo_renew()
                 self.dic_renew()
-                
-                break
             
-            elif len( set( self.Dic[ key ] ) - set( block_test ) ) == 1:
-                self.itself[ key//9 ][ key%9 ]  = list( set( self.Dic[ key ] ) - set( block_test ) )[ 0 ]
+            elif len( block_test ) == 1:
+                self.itself[ key//9 ][ key%9 ]  = block_test[ 0 ]
                 
                 self.had_insert = True
                 self.sudo_renew()
                 self.dic_renew()
-                
+
+
+def print_sudo_which_is( my_sudo : sudoku ):
+    for i in my_sudo.itself:
+        print( i )
+
 
 def fix( my_sudo : sudoku ):
     while ( my_sudo.can_be_solve() ):
-        
         my_sudo.simple_insert()
         my_sudo.complex_insert()
         
         if ( not( my_sudo.had_insert ) and my_sudo.the_min_of_dic() > 1):
-            break
-        
+            break 
+    
     return my_sudo
 
+def assume_number_from( my_sudo : sudoku ):
+    global answer_of_my_sudo
+    
+    my_sudo.sudo_renew()
+    my_sudo.dic_renew()
+    my_sudo.the_min_of_dic()
+    place = my_sudo.the_possible_place
+    
+    for i in range( len( my_sudo.Dic[ place ] ) ):
+        
+        my_clone_sudo = copy.deepcopy( my_sudo )
+        my_clone_sudo.itself[ place // 9][ place % 9 ] = my_sudo.Dic[ place ][ i ]
+        
+        my_clone_sudo.sudo_renew()
+        my_clone_sudo.dic_renew()
+        
+        my_assume_sudo = copy.deepcopy( fix( my_clone_sudo ) )
+        
+        if my_assume_sudo.need_to_solve():
+            
+            if my_assume_sudo.can_be_solve():
+                
+                if assume_number_from( my_assume_sudo ):
+                    return True
+                
+            else:
+                if i == len( my_sudo.Dic[ place ] ):
+                    return False
+        else:
+            answer_of_my_sudo = my_assume_sudo
+            return True
+    return False
+     
 def main():
-    my_sudo = sudoku( [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-                       [6, 0, 0, 1, 9, 5, 0, 0, 0],
-                       [0, 9, 8, 0, 0, 0, 0, 6, 0],
-                       [8, 0, 0, 0, 6, 0, 0, 0, 3],
-                       [4, 0, 0, 8, 0, 3, 0, 0, 1],
-                       [7, 0, 0, 0, 2, 0, 0, 0, 6],
-                       [0, 6, 0, 0, 0, 0, 2, 8, 0],
-                       [0, 0, 0, 4, 1, 9, 0, 0, 5],
-                       [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    my_sudo = sudoku( [[8, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 3, 6, 0, 0, 0, 0, 0],
+                       [0, 7, 0, 0, 9, 0, 2, 0, 0],
+                       [0, 5, 0, 0, 0, 7, 0, 0, 0],
+                       [0, 0, 0, 0, 4, 5, 7, 0, 0],
+                       [0, 0, 0, 1, 0, 0, 0, 3, 0],
+                       [0, 0, 1, 0, 0, 0, 0, 6, 8],
+                       [0, 0, 8, 5, 0, 0, 0, 1, 0],
+                       [0, 9, 0, 0, 0, 0, 4, 0, 0]
                        ])
-
-    my_sudo.can_be_solve()
-    
-    my_sudo = fix(my_sudo)
-    
-    print(my_sudo.can_be_solve())
-    for i in range( 9 ):
-        print( my_sudo.itself[ i ] )
-    for i in my_sudo.Dic:
-        print(i, my_sudo.Dic[ i ])
+    if my_sudo.need_to_solve():
+        my_sudo = fix( my_sudo ) 
         
+        if my_sudo.can_be_solve() :
+            answer = assume_number_from( my_sudo )
+            print( "answer:", answer )
+            
+            if answer:
+                my_sudo = answer_of_my_sudo
+                print( "my_sudo.can_be_solve()", my_sudo.can_be_solve() )
+                print_sudo_which_is( my_sudo )
+                
+            else:
+                print( "answer:", answer )
+        else:
+            print( my_sudo.can_be_solve() )
+            print_sudo_which_is( my_sudo )
+    else:
+        print_sudo_which_is( my_sudo )
     
-        
-    
-main()
+         
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print( end - start )
